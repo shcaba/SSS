a4770ee3b5049ef73eff43c48c7f2950d86f0c24
Jason Cope <jason.cope@noaa.gov>
2018-05-24 16:40:55 -0700

    Add "sd" to file names

--- SSS_code_newSRs.r
+++ SSS_code_newSRs.r
@@ -73,9 +73,9 @@ r.beta.solve<-function(sd.init,mean.init,mean.match,sd.match,seedit,logn=T)
 }
 
 #Solve for the SD to match rbeta to either lognormal or truncated normal
-opt.s.prof<-function(mean.val,mean.match,sd.match,seedit,logn=T)
+opt.sd.prof<-function(mean.val,mean.match,sd.match,seedit,logn=T)
 {
-  obj.best<-optimize(r.beta.solve,c(0, 0.6), tol = 0.0001,mean.init=mean.val,mean.match=mean.match,sd.match=sd.match,seedit=seedit,logn=logn)[[1]]
+  obj.best<-optimize(r.beta.solve,c(0, 1), tol = 0.0001,mean.init=mean.val,mean.match=mean.match,sd.match=sd.match,seedit=seedit,logn=logn)[[1]]
   return(obj.best)
 }
 
